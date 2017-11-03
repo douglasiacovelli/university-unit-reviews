694fa17ebc1b4849851981124e460009e3749816
Douglas Iacovelli <douglas.iacovelli@gmail.com>
2017-11-03 00:29:00 -0200

    starts to create form for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,22 +9,50 @@ class ReviewsController < ApplicationController
   end
 
   def new
+    @review = Review.new
   end
 
   def edit
   end
 
   def create
+    @review = Review.create(review_params)
+
+    respond_to do |format|
+      if @review.save
+        format.html { redirect_to @review, notice: 'Avaliação salva com sucesso.' }
+        format.json { render :show, status: :created, location: @review}
+      else
+        format.html { render :new }
+        format.json { render json: @review.errors, status: :unprocessable_entity }
+    end
+
+    end
   end
 
   def update
+    respond_to do |format|
+      if @review.update(review_params)
+        format.html { redirect_to @review, notice: 'Review atualizada' }
+        format.json { render :show, status: :ok, location: @review }
+      else
+        format.html { render :edit }
+        format.json { render json: @review.errors, status: :unprocessable_entity }
+
+      end
+    end
   end
 
   def destroy
+    @review.destroy
+
+    respond_to do |format|
+      format.html { redirect_to courses_url, notice: 'Course was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
-
     def set_review
       @review = Review.find(params[:id])
     end
